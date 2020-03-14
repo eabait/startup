42a5a96dbc5b16c59421b2a4c31bcf56080e5c50
Matias Galvan <matiasgalvan92@gmail.com>
2020-03-13 22:09:40 -0300

    5. ES6 Modules

---
+++ 02-oop-inheritance/5. ES6 Modules/src/Movie.js
@@ -0,0 +1,30 @@
+class Movie extends EventEmitter{
+    constructor(title, year, duration){
+        super();
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+        this.cast = [];
+    }
+    play(){
+        super.emit('play');
+    }
+    pause(){
+        super.emit('pause');
+    }
+    resume(){
+        super.emit('resume');
+    }
+    addCast(cast){
+        if(Array.isArray(cast)){
+            for(let i=0; i<cast.length; i++){
+                if(cast[i] instanceof Actor){
+                    this.cast.push(cast[i]);
+                }
+            }
+        } else if(cast instanceof Actor){
+            this.cast.push(cast);
+        }
+        
+    }
+}
