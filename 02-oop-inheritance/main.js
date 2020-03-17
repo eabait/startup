c8034d38bd1f2c48b229858cea764b922d2b9e78
Luciano_Sassano <lucianosassano1@gmail.com>
2020-03-16 23:46:10 -0300

    item 2

--- 02-oop-inheritance/main.js
+++ 02-oop-inheritance/main.js
@@ -1,43 +1,73 @@
 class EventEmitter {
-  constructor() {}
+  constructor() {
+    this.events = {};
+  }
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
-  on(eventName, callback) {}
-  emit(eventName, callback) {}
-  delete(eventName, callback) {}
+  emit(eventName, ...rest) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(cb => {
+        cb.apply(rest);
+      });
+    }
+  }
+//   deleteSingle(eventName, callback) {
+//     return () =>{
+//         this.events[eventName] = this.events[eventName].filter(eventCb => callback!== eventCb )
+//     }
+// in the function shown above you pass a one and remove that one  ,in the function below you pass one 
+// and "filter out" the rest
+//}
+
+  delete(eventName, callback) {
+      return () =>{
+          this.events[eventName] = this.events[eventName].filter(eventCb => callback === eventCb )
+      }
+  }
 }
 
-class Movie {
+class Movie extends EventEmitter {
   constructor(title, year, duration) {
+    super();
     this.title = title;
     this.year = year;
     this.duration = duration;
   }
 
   play() {
-    console.log(this.title, "is currently playing");
+    this.emit("play");
   }
 
   pause() {
-    console.log(this.title, "is currently paused");
+    this.emit("pause");
   }
 
   resume() {
-    
-    console.log(this.title, " resumed");
+    this.emit("resume");
   }
 }
 
 //Class instance and testing methods
 
-let movie1 = new Movie("avenger", 2018, 149);
-movie1.play();
-movie1.pause();
-movie1.resume();
+let movie1 = new Movie("avengers", 2018, 149);
 
-let movie2 = new Movie("sonic", 2019, 135);
-movie2.play();
-movie2.pause();
-movie2.resume();
+movie1.on("play", () => {
+  console.log("play event trigered");
+});
+movie1.on("pause", () => {
+  console.log("pause event trigered");
+});
+movie1.on("resume", () => {
+  console.log("resume event trigered");
+});
+
+movie1.play()
 
 class Actor {
   constructor(name, age) {
