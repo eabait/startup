1a9d3de5eb52abd7d0e2087c532282b18063f8a2
Luciano_Sassano <lucianosassano1@gmail.com>
2020-03-17 15:27:48 -0300

    item 3

--- 02-oop-inheritance/main.js
+++ 02-oop-inheritance/main.js
@@ -17,18 +17,21 @@ class EventEmitter {
       });
     }
   }
-//   deleteSingle(eventName, callback) {
-//     return () =>{
-//         this.events[eventName] = this.events[eventName].filter(eventCb => callback!== eventCb )
-//     }
-// in the function shown above you pass a one and remove that one  ,in the function below you pass one 
-// and "filter out" the rest
-//}
+
+  deleteSingle(eventName, callback) {
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(
+        eventCb => callback !== eventCb
+      );
+    };
+  }
 
   delete(eventName, callback) {
-      return () =>{
-          this.events[eventName] = this.events[eventName].filter(eventCb => callback === eventCb )
-      }
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(
+        eventCb => callback === eventCb
+      );
+    };
   }
 }
 
@@ -38,6 +41,7 @@ class Movie extends EventEmitter {
     this.title = title;
     this.year = year;
     this.duration = duration;
+    this.cast = [];
   }
 
   play() {
@@ -51,23 +55,23 @@ class Movie extends EventEmitter {
   resume() {
     this.emit("resume");
   }
+
+  addCast(param) {
+    if (Array.isArray(param)) {
+      for (let i = 0; i < param.length; i++) {
+        this.cast.push(param[i]);
+      }
+    } else {
+      this.cast.push(param);
+    }
+  }
+
+  showCast() {
+    for (let i = 0; i < this.cast.length; i++) {
+      console.log(this.cast[i]);
+    }
+  }
 }
-
-//Class instance and testing methods
-
-let movie1 = new Movie("avengers", 2018, 149);
-
-movie1.on("play", () => {
-  console.log("play event trigered");
-});
-movie1.on("pause", () => {
-  console.log("pause event trigered");
-});
-movie1.on("resume", () => {
-  console.log("resume event trigered");
-});
-
-movie1.play()
 
 class Actor {
   constructor(name, age) {
@@ -75,3 +79,45 @@ class Actor {
     this.age = age;
   }
 }
+
+class Logger {
+  constructor() {}
+  log(info) {
+    if (Array.isArray(info)) {
+      for (let i = 0; i < info.length; i++) {
+        console.log(info[i]);
+      }
+    } else {
+      console.log("the " + info + " event has been triggered");
+    }
+  }
+}
+
+//Class instance and testing methods
+
+let movie1 = new Movie("avengers", 2018, 149);
+
+let logger = new Logger();
+
+movie1.on("play", () => {
+  logger.log("play");
+});
+movie1.on("pause", () => {
+  logger.log("pause");
+});
+movie1.on("resume", () => {
+  logger.log("resume");
+});
+
+movie1.play();
+
+const robert = new Actor("Robert Downey Jr", 47);
+const actors = [
+  new Actor("Chris Evans", 38),
+  new Actor("Mark Ruffalo", 52),
+  new Actor("Scarlett Johansson", 35)
+];
+
+movie1.addCast(robert);
+movie1.addCast(actors);
+movie1.showCast();
